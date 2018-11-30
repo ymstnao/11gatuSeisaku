935fd00d562054ec637db0b020971c0ec9161915
Ainzusama <44426573+Ainzusama@users.noreply.github.com>
2018-11-30 13:29:03 +0900

    Save

--- Assets/Script/SavePoint.cs
+++ Assets/Script/SavePoint.cs
@@ -3,20 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SavePoint : MonoBehaviour {
+    [SerializeField,Header("浮遊する高さ")]
+    private float floatingY;
+    [SerializeField, Header("浮遊する速さ")]
+    private float floatingSpeed;
     private int id;
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    private Vector3 defaultPosition;
+    // Use this for initialization
+    void Start() {
+        defaultPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update() {
         Rotation();
+        Floating();
     }
     void Rotation()
     {
         transform.Rotate(1, 1, 0);
     }
+
+    /// <summary>
+    /// オブジェクトをふよふよさせる
+    /// </summary>
+    void Floating() {
+        var position = defaultPosition;
+        
+        position.y += Mathf.Sin(Time.timeSinceLevelLoad * floatingSpeed) * floatingY;
+
+        transform.position = position;
+    }
+
     public void Save(PlayerStatus status)
     {
         status.HpChange(status.MaxHp);
